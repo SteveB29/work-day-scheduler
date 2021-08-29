d967fa9dab1d91b4d7fd02ec99ee4761c0c4f560
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-29 14:41:18 -0400

    fixed class not updating after hour passed

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ $('#currentDay').text(today);
 
 
 var checkTime = function() {
+    var hour = moment().format("H");
+    var hourInt = parseInt(hour);
     for (i=9; i < 18; i++) {
-        var hour = moment().format("H");
-        var hourInt = parseInt(hour);
         if (i < hourInt) {
+            $('#'+i).removeClass('past present future');
             $('#'+i).addClass('past');
         }
         else if (i === hourInt) {
+            $('#'+i).removeClass('past present future');
             $('#'+i).addClass('present');
         }
         else {
+            $('#'+i).removeClass('past present future');
             $('#'+i).addClass('future');
         }
     }
