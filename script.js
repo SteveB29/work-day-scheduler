49fdd4ee181e2c58ee00e548bb9435ed01622cec
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-29 13:48:09 -0400

    added all timeblocks and color coding logic

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 var today = moment().format("dddd, MMMM Do");
-console.log(today);
-
 $('#currentDay').text(today);
+
+
+var checkTime = function() {
+    for (i=9; i < 18; i++) {
+        var hour = moment().format("H");
+        var hourInt = parseInt(hour);
+        if (i < hourInt) {
+            $('#'+i).addClass('past');
+        }
+        else if (i === hourInt) {
+            $('#'+i).addClass('present');
+        }
+        else {
+            $('#'+i).addClass('future');
+        }
+    }
+};
+
+checkTime();
+setInterval(function() {
+    checkTime();
+    console.log("time checked");
+}, 60000);
